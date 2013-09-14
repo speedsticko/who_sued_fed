7cf528025e7d716f60e11af823b0a7696aafb6ca
Sergey Vershinin <svershin@gmail.com>
2013-09-14 17:50:34 -0400

    Data objects working, can now move on to rendering the chart

--- routes/index.js
+++ routes/index.js
@@ -2,54 +2,73 @@
  * Routes for CanLII API Demo
  */
 
-function index (req, res) {
+exports.index = function (req, res) {
 
-  // Require http to enable querying the CanLII API
-  var http = require("http");
+  // Require request module for simplified HTTP GET requests
+  var request = require("request");
 
   // Define API request parameters
   var reqParameters = {
-    api_key: 'c9jckm4neq4nhancmd6pbqmd',
-    offset: 0,
-    resultCount: 10,
-    fullText: 'citizenship+and+immigration'
+      api_key: 'c9jckm4neq4nhancmd6pbqmd',
+      offset: 0,
+      resultCount: 100,
+      fullText: 'citizenship+and+immigration'
     },
-    reqPath = '/v1/search/en/?';
+    reqPath = 'http://api.canlii.org/v1/search/en/?';
 
+  // Concatinate request parameters onto the API request path
   for (param in reqParameters) {
     if (reqParameters.hasOwnProperty(param)) {
       reqPath += param + '=' + reqParameters[param] + '&';
     }
   }
-
   reqPath = reqPath.slice(0,reqPath.length-1);
 
-// res.end(reqPath);
+  // Define callback for the API request
+  function callback(error, response, body)  {
+    if (!error && response.statusCode == 200) {
 
-  // Define HTTP request parameters
-  var options = {
-    host: 'api.canlii.org',
-    port: 80,
-    path: reqPath
-  };
+      // Create data object to be returned and
+      // the regexes for identifying defendant and plaintif cases
+      var data_object = {},
+          patt_d = /v[.].+Canada.+Citizenship\s+and\s+Immigration/,
+          patt_p = /Canada.+Citizenship\s+and\s+Immigration.+v[.].+/;
 
-  // Send a GET request to CanLII
-  http.get(options, function(apiResponse) {
+      console.log(body);
 
-    // Output response status to console
-    console.log("Got response: " + apiResponse.statusCode);
+      // Parse API response and populate the data object
+      for (var i=0; i<body['results'].length-1; i++) {
 
-    // Add a listener for the 'data' event in the response
-    apiResponse.on("data", function(chunk) {
-      res.end("BODY: " + chunk);
-    });
+        // Filter out results that are not cases
+        if(body['results'][i].hasOwnProperty('case')) {
 
-  // Error handler for the GET request
-  }).on('error', function(e) {
+          // determine year from caseId ***DANGEROUS*** may not work for all cases
+          var year = body['results'][i]["case"]["caseId"]["en"].substring(0,4),
+          // init var for to as pointer for counting
+              currRecord = null;
 
-    res.end("Got error: " + e.message);
+          if(data_object[year]){
+            currRecord = data_object[year];
+          } else {
+            data_object[year] = { d : 0, p : 0}
+            currRecord = data_object[year];
+          }
 
-  });
-};
+          if(patt_d.test(body['results'][i]["case"]["title"])) {
+            currRecord.d += 1;
+          }
+          else if(patt_d.test(body['results'][i]["case"]["title"])) {
+            currRecord.p += 1;
+          }
+        }
+      }
 
-exports.index = index;
+      // Return data object to requestor (i.e. browser)
+      res.end(JSON.stringify(data_object, null, "\t"));
+
+    }
+  }
+
+  // Make the request to CanLII API
+  request({url: reqPath, json: true}, callback);
+}
